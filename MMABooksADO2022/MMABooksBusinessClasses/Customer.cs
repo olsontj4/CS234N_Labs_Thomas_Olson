ab855d37b5f52b1c6124bc4c7e7390211d92179a
thomasj041 <thomas.josholson@gmail.com>
2024-10-17 19:33:19 -0700

    Customer class tests complete.

---
+++ MMABooksADO2022/MMABooksBusinessClasses/Customer.cs
@@ -0,0 +1,173 @@
+﻿using System;
+
+namespace MMABooksBusinessClasses
+{
+    public class Customer
+    {
+        // there are several warnings in this file related to nullable properties and return values.
+        // you can ignore them
+        public Customer() { }
+
+        public Customer(int id, string name, string address, string city, string state, string zipcode)
+        {
+            CustomerID = id;
+            Name = name;
+            Address = address;
+            City = city;
+            State = state;
+            ZipCode = zipcode;
+        }
+
+        private int customerID;
+        private string name;
+        private string address;
+        private string city;
+        private string state;
+        private string zipcode;
+
+        public int CustomerID
+        {
+            get
+            {
+                return customerID;
+            }
+            set
+            {
+                if (value > 0)
+                {
+                    customerID = value;
+                }
+                else
+                {
+                    throw new ArgumentOutOfRangeException("Customer ID must be a positive integer.");
+                }
+            } 
+        }
+
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                if (value.Trim().Length > 0)
+                {
+                    if (value.Trim().Length <= 100)
+                    {
+                        name = value.Trim();
+                    }
+                    else
+                    {
+                        throw new ArgumentOutOfRangeException("Character limit is 100.");
+                    }
+                }
+                else
+                {
+                    throw new ArgumentOutOfRangeException("String is empty.");
+                }
+            }
+        }
+
+        public string Address
+        {
+            get
+            {
+                return address;
+            }
+            set
+            {
+                if (value.Trim().Length > 0)
+                {
+                    if (value.Trim().Length <= 50)
+                    {
+                        address = value.Trim();
+                    }
+                    else
+                    {
+                        throw new ArgumentOutOfRangeException("Character limit is 50.");
+                    }
+                }
+                else
+                {
+                    throw new ArgumentOutOfRangeException("String is empty.");
+                }
+            }
+        }
+
+        public string City
+        {
+            get
+            {
+                return city;
+            }
+            set
+            {
+                if (value.Trim().Length > 0)
+                {
+                    if (value.Trim().Length <= 20)
+                    {
+                        city = value.Trim();
+                    }
+                    else
+                    {
+                        throw new ArgumentOutOfRangeException("Character limit is 20.");
+                    }
+                }
+                else
+                {
+                    throw new ArgumentOutOfRangeException("String is empty.");
+                }
+            }
+        }
+
+        public string State
+        {
+            get
+            {
+                return state;
+            }
+            set
+            {
+                if (value.Trim().ToUpper().Length == 2)
+                {
+                    state = value.Trim().ToUpper();
+                }
+                else
+                {
+                    throw new ArgumentOutOfRangeException("State must be 2 characters.");
+                }
+            }
+        }
+        public string ZipCode
+        {
+            get
+            {
+                return zipcode;
+            }
+            set
+            {
+                if (value.Trim().Length >= 5)
+                {
+                    if (value.Trim().Length <= 15)
+                    {
+                        zipcode = value.Trim();
+                    }
+                    else
+                    {
+                        throw new ArgumentOutOfRangeException("Character limit is 15.");
+                    }
+                }
+                else
+                {
+                    throw new ArgumentOutOfRangeException("Zip code must be at least 5 characters.");
+                }
+            }
+        }
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+    }
+}
---
+++ MMABooksADO2022/MMABooksTests/CustomerTests.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using NUnit.Framework;
+using MMABooksBusinessClasses;
+
+namespace MMABooksTests
+{
+    [TestFixture]
+    public class CustomerTests
+    {
+        private Customer def;
+        private Customer c;
+
+        [SetUp]
+
+        public void Setup()
+        { 
+            def = new Customer(); 
+            c = new Customer(1, "Olson, Thomas", "136 Oakway Center", "Eugene", "OR", "97401");
+        }
+
+        [Test]
+        public void TestConstructor() 
+        {
+            Assert.IsNotNull(def);
+            Assert.AreEqual(0, def.CustomerID);
+            Assert.AreEqual(null, def.Name);
+            Assert.AreEqual(null, def.Address);
+            Assert.AreEqual(null, def.City);
+            Assert.AreEqual(null, def.State);
+            Assert.AreEqual(null, def.ZipCode);
+
+            Assert.IsNotNull(c);
+            Assert.AreNotEqual(0, c.CustomerID);
+            Assert.AreNotEqual(null, c.Name);
+            Assert.AreNotEqual(null, c.Address);
+            Assert.AreNotEqual(null, c.City);
+            Assert.AreNotEqual(null, c.State);
+            Assert.AreNotEqual(null, c.ZipCode);
+        }
+
+        [Test]
+        public void TestNameSetter()
+        {
+            c.Name = "Olson, Samoht";
+            Assert.AreNotEqual("Olson, Thomas", c.Name);
+            Assert.AreEqual("Olson, Samoht", c.Name);
+        }
+
+        [Test]
+        public void TestNameTooLong()
+        {
+            Assert.Throws<ArgumentOutOfRangeException>(() => c.Name = "skjkdfjgkdfljghdkfjghkldjghkerjhkrjgkfjgkdfjgkjdfgkjdfhgkjoirjgriljeinjnkjfgkdfjnglskfnlfsndlkfndsklfjlksdjflknslanvnprwoturiohlskjd;;kanajksnfjengijrgoroejgiroi");
+        }
+
+        [Test]
+        public void TestNameEmpty()
+        {
+            Assert.Throws<ArgumentOutOfRangeException>(() => c.Name = "");
+        }
+
+        [Test]
+        public void TestAddressSetter()
+        {
+            c.Address = "101 Oakway Center";
+            Assert.AreNotEqual("136 Oakway Center", c.Address);
+            Assert.AreEqual("101 Oakway Center", c.Address);
+        }
+
+        [Test]
+        public void TestAddressTooLong()
+        {
+            Assert.Throws<ArgumentOutOfRangeException>(() => c.Address = "skjkdfjgkdfljghdkfjghkldjghkerjhkrjgkfjgkdfjgkjdfgkjdfhgkjoirjgriljeinjnkjfgkdfjnglskfnlfsndlkfndsklfjlksdjflknslanvnprwoturiohlskjd;;kanajksnfjengijrgoroejgiroi");
+        }
+
+        [Test]
+        public void TestAddressEmpty()
+        {
+            Assert.Throws<ArgumentOutOfRangeException>(() => c.Address = "");
+        }
+
+        [Test]
+        public void TestCitySetter()
+        {
+            c.City = "Springfield";
+            Assert.AreNotEqual("Eugene", c.City);
+            Assert.AreEqual("Springfield", c.City);
+        }
+
+        [Test]
+        public void TestCityTooLong()
+        {
+            Assert.Throws<ArgumentOutOfRangeException>(() => c.City = "skjkdfjgkdfljghdkfjghkldjghkerjhkrjgkfjgkdfjgkjdfgkjdfhgkjoirjgriljeinjnkjfgkdfjnglskfnlfsndlkfndsklfjlksdjflknslanvnprwoturiohlskjd;;kanajksnfjengijrgoroejgiroi");
+        }
+
+        [Test]
+        public void TestCityEmpty()
+        {
+            Assert.Throws<ArgumentOutOfRangeException>(() => c.City = "");
+        }
+
+        [Test]
+        public void TestStateSetter()
+        {
+            c.State = "CA";
+            Assert.AreNotEqual("OR", c.State);
+            Assert.AreEqual("CA", c.State);
+        }
+
+        [Test]
+        public void TestStateIncorrectCharacters()
+        {
+            Assert.Throws<ArgumentOutOfRangeException>(() => c.State = "ore");
+            Assert.Throws<ArgumentOutOfRangeException>(() => c.State = "o");
+        }
+
+        [Test]
+        public void TestZipCodeSetter()
+        {
+            c.ZipCode = "97402";
+            Assert.AreNotEqual("97401", c.ZipCode);
+            Assert.AreEqual("97402", c.ZipCode);
+        }
+
+        [Test]
+        public void TestZipCodeTooLong()
+        {
+            Assert.Throws<ArgumentOutOfRangeException>(() => c.ZipCode = "9283993300282309763");
+        }
+
+        [Test]
+        public void TestZipCodeTooShort()
+        {
+            Assert.Throws<ArgumentOutOfRangeException>(() => c.ZipCode = "1234");
+        }
+    }
+}
