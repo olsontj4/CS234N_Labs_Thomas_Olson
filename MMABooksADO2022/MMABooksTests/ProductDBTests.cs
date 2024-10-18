c93e0c47bd1782a2e382e3ae4d1d2c2dadbd99e0
thomasj041 <thomas.josholson@gmail.com>
2024-10-18 16:48:36 -0700

    Lab 2 in-progress.

--- MMABooksADO2022/MMABooksTests/ProductDBTests.cs
+++ MMABooksADO2022/MMABooksTests/ProductDBTests.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using MMABooksBusinessClasses;
+using MMABooksDBClasses;
+using NUnit.Framework;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,5 +9,23 @@ namespace MMABooksTests
 {
     public class ProductDBTests
     {
+
+        /*[Test]
+        public void TestGetProduct()
+        {
+            Product p = ProductDB.GetProduct(1);
+            Assert.AreEqual(1, p.ProductID);
+        }
+
+        [Test]
+        public void TestCreateProduct()
+        {
+            Product p = new Product();
+            p.Name = "";
+
+            int productID = ProductDB.AddProduct(p);
+            p = ProductDB.GetProduct(productID);
+            Assert.AreEqual("", p.Name);
+        }*/
     }
 }
